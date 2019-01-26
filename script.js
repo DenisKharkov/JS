9612f96783fb4cf9b6f0c22b390b9137fb488589
DenisKharkov <denis-kharkov@inbox.ru>
2019-01-26 11:45:53 +0200

    ДЗ по первому уроку JS готово

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-var a = 20, b = 20;
-
-text = a > b ? a + ' больше ' + b : a < b ? a + ' меньше ' + b : a + ' равно ' + b;
-
-console.log(text);
-
-function condition (a , b) {
- return a > b ? a + ' больше ' + b : a < b ? a + ' меньше ' + b : a + ' равно ' + b;
-}
-
-console.log(condition (10, 20));
-
-console.log(condition (20, 20));
-
-console.log(condition (30, 20));
-
-
-
-const condition1 = (a , b) => a > b ? `${a} больше ${b}` : a < b ? `${a} меньше  ${b}` : `${a} равно ${b}`;
+var a = 'Lorem ipsum dolor sit amet', 
+b = 25;
+c = false;
+d = {name: 'TestObject'};
+e = [2, 4, 6, 8, 10];
+i = b*2;
 
 
 
 
 
-console.log(condition1 (10, 20));
 
-console.log(condition1 (20, 20));
 
-console.log(condition1 (30, 20));
+
+console.log(a, b, c, d, e);
+console.log(b.toPrecision(4));
+console.log(i);
+console.log(++i);
+console.log(i);
+console.log(i++);
+
+console.log(b += 10);
+console.log(b -= 10);
+console.log(b *= 10);
+console.log(b /= 10);
+console.log(b %= 10);
+
+console.log('Число ПИ', Math.PI);
+console.log('Округление', Math.round(89.279));
+console.log('Число от 1 до 10', Math.random()*10);
+console.log('2 в степени 5', Math.pow(2,5));
+
+console.log('Длина строки', a.length);
+console.log('Ищем символы в строке', a.indexOf('sit'));
+console.log('Заменяем символы в строке', a.replace('Lorem', 'Dorem'));
+console.log('Меняем верхний и нижний регистры', a.toUpperCase(), a.toLowerCase());
+
+
+
 
